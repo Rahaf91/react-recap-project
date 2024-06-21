9937f529640a489e0f7474f1ad00184f42efaa9b
Rahaf <rahafsekny@gmail.com>
2024-06-21 09:53:34 +0200

    update theme.jsx

--- src/Components/Theme/Theme.jsx
+++ src/Components/Theme/Theme.jsx
@@ -7,26 +7,21 @@ export default function Theme({
   handleDeleteColor,
   handleEditColor,
 }) {
-  if (!colors || colors.length === 0) {
-    return (
-      <div>
-        <ColorForm text={"Add Color"} handleAddColor={handleAddColor} />
-        <p className="message">Let it shine again! Please add new colors!</p>
-      </div>
-    );
-  }
-
   return (
     <div>
       <ColorForm text={"Add Color"} handleAddColor={handleAddColor} />
-      {colors.map((color) => (
-        <Color
-          key={color.id}
-          color={color}
-          handleDeleteColor={handleDeleteColor}
-          handleEditColor={handleEditColor}
-        />
-      ))}
+      {!colors || colors.length === 0 ? (
+        <p className="message">Let it shine again! Please add new colors!</p>
+      ) : (
+        colors.map((color) => (
+          <Color
+            key={color.id}
+            color={color}
+            handleDeleteColor={handleDeleteColor}
+            handleEditColor={handleEditColor}
+          />
+        ))
+      )}
     </div>
   );
 }
